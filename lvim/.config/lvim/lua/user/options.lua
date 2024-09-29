39cadedd186033a52a3ef74612175991f14aef11
tory <5239604@qq.com>
2024-09-30 00:38:36 +0800

    feat(fish): custom spell file

--- lvim/.config/lvim/lua/user/options.lua
+++ lvim/.config/lvim/lua/user/options.lua
@@ -1,7 +1,10 @@
+local conf_dir                  = vim.fn.stdpath("config")
+
 lvim.transparent_window         = true
 lvim.colorscheme                = 'solarized-osaka'
 vim.opt.relativenumber          = true
 vim.opt.spell                   = true
+vim.opt.spellfile               = conf_dir .. "/spell/en.utf-8.add"
 
 -- ignore other pattern
 lvim.builtin.project.patterns   = { ".git" }

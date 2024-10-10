ffe0b2d417bf32806fea0a0686fe1c6ee2cc07e6
tory <5239604@qq.com>
2024-10-10 09:49:21 +0800

    feat(terminal): use wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -11,13 +11,16 @@ config.window_padding = {
   bottom = 0,
   left = 8,
 }
-config.color_scheme = "AdventureTime"
-config.font = wezterm.font_with_fallback({  "FiraCode Nerd Font Mono", "Noto Color Emoji", "WenQuanYi Micro Hei Mono" })
+-- config.color_scheme = "AdventureTime"
+config.font = wezterm.font_with_fallback(
+  { "FiraCode Nerd Font Mono",
+    "Noto Color Emoji",
+    "WenQuanYi Micro Hei Mono" })
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.80
 config.initial_cols = 120
 config.initial_rows = 36
-config.font_size = 14
+-- config.font_size = 14
 -- config.background = {
 -- 	height = "1cell",
 -- 	opacity = 0.75,
